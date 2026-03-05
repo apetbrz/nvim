13679c9fadb8cd9eeee57a284cc0988c360cbae4
Arthur Petroff <apetbrz@gmail.com>
2026-03-05 14:25:06 -0500

    big binding changes

--- lua/config.lua
+++ lua/config.lua
@@ -24,17 +24,58 @@ vim.keymap.set("n", "H", ":bprevious<cr>", { silent = true, desc = "Previous buf
 vim.keymap.set("n", "L", ":bnext<cr>", { silent = true, desc = "Next buffer" })
 vim.keymap.set("n", "<leader>q", "<cmd>bdelete<cr>", { silent = true, desc = "Close buffer" })
 -- splits
-vim.keymap.set('n', '<leader>s', '', { silent = true, desc = "Split" })
-vim.keymap.set('n', '<leader>sv', ':vsplit<cr>:bnext<cr>', { silent = true, desc = "Split onto right" })
+vim.keymap.set('n', '<leader>S', '', { silent = true, desc = "Split" })
+vim.keymap.set('n', '<leader>Sv', ':vsplit<cr>:bnext<cr>', { silent = true, desc = "Split onto right" })
+
+-- treesitter support --
+require("nvim-treesitter-textobjects").setup({ select = { lookahead = true, include_surrounding_whitespace = false } })
+local tssel = require("nvim-treesitter-textobjects.select")
+local tsmv = require("nvim-treesitter-textobjects.move")
+local tsswp = require("nvim-treesitter-textobjects.swap")
+-- textobjs
+vim.keymap.set({ "x", "o" }, "af", function() tssel.select_textobject("@function.outer", "textobjects") end, { desc = "function" })
+vim.keymap.set({ "x", "o" }, "if", function() tssel.select_textobject("@function.inner", "textobjects") end, { desc = "inner function" })
+vim.keymap.set({ "x", "o" }, "aa", function() tssel.select_textobject("@parameter.outer", "textobjects") end, { desc = "argument" })
+vim.keymap.set({ "x", "o" }, "ia", function() tssel.select_textobject("@parameter.inner", "textobjects") end, { desc = "inner argument" })
+vim.keymap.set({ "x", "o" }, "ac", function() tssel.select_textobject("@call.outer", "textobjects") end, { desc = "function call" })
+vim.keymap.set({ "x", "o" }, "ic", function() tssel.select_textobject("@call.inner", "textobjects") end, { desc = "inner function call" })
+vim.keymap.set({ "x", "o" }, "ar", function() tssel.select_textobject("@class.outer", "textobjects") end, { desc = "struct" })
+vim.keymap.set({ "x", "o" }, "ir", function() tssel.select_textobject("@class.inner", "textobjects") end, { desc = "inner struct" })
+vim.keymap.set({ "x", "o" }, "as", function() tssel.select_textobject("@block.outer", "textobjects") end, { desc = "scope" })
+vim.keymap.set({ "x", "o" }, "is", function() tssel.select_textobject("@block.inner", "textobjects") end, { desc = "inner scope" })
+-- moves
+vim.keymap.set({ "n", "x", "o" }, ";", function() require("nvim-treesitter-textobjects.repeatable_move").repeat_last_move() end)
+vim.keymap.set({ "n", "x", "o" }, ",", function() require("nvim-treesitter-textobjects.repeatable_move").repeat_last_move_opposite() end)
+vim.keymap.set('n', '<leader>n', '', { silent = true, desc = "Next" })
+vim.keymap.set("n", "<leader>na", function() tsmv.goto_next_start("@parameter.inner", "textobjects") end, { desc = "argument" })
+vim.keymap.set("n", "<leader>nf", function() tsmv.goto_next_start("@function.outer", "textobjects") end, { desc = "function" })
+vim.keymap.set("n", "<leader>nb", function() tsmv.goto_next_start("@function.outer", "textobjects") end, { desc = "function body" })
+vim.keymap.set("n", "<leader>nc", function() tsmv.goto_next_start("@call.outer", "textobjects") end, { desc = "function call" })
+vim.keymap.set("n", "<leader>nr", function() tsmv.goto_next_start("@class.outer", "textobjects") end, { desc = "struct definition" })
+vim.keymap.set("n", "<leader>ns", function() tsmv.goto_next_start("@block.inner", "textobjects") end, { desc = "scope" })
+vim.keymap.set('n', '<leader>p', '', { silent = true, desc = "Previous" })
+vim.keymap.set("n", "<leader>pa", function() tsmv.goto_previous_start("@parameter.inner", "textobjects") end, { desc = "argument" })
+vim.keymap.set("n", "<leader>pd", function() tsmv.goto_previous_start("@function.outer", "textobjects") end, { desc = "function" })
+vim.keymap.set("n", "<leader>pc", function() tsmv.goto_previous_start("@call.outer", "textobjects") end, { desc = "function call" })
+vim.keymap.set("n", "<leader>pr", function() tsmv.goto_previous_start("@class.outer", "textobjects") end, { desc = "struct" })
+vim.keymap.set("n", "<leader>ps", function() tsmv.goto_previous_start("@block.inner", "textobjects") end, { desc = "scope" })
+vim.keymap.set("n", "<leader>u", function() require("treesitter-context").go_to_context(vim.v.count1) end, { desc = "Up in context" })
+-- swaps
+vim.keymap.set({ 'n', "v" }, '<leader>s', '', { silent = true, desc = "Swap (Treesitter)" })
+vim.keymap.set({ "n", "v" }, "<leader>sa", function() tsswp.swap_next("@parameter.inner", "textobjects") end, { desc = "next argument" })
+vim.keymap.set({ "n", "v" }, "<leader>sA", function() tsswp.swap_previous("@parameter.inner", "textobjects") end, { desc = "prev argument" })
+vim.keymap.set({ "n", "v" }, "<A-l>", function() tsswp.swap_next("@parameter.inner", "textobjects") end, { desc = "swap next argument" })
+vim.keymap.set({ "n", "v" }, "<A-h>", function() tsswp.swap_previous("@parameter.inner", "textobjects") end, { desc = "swap prev argument" })
 
 -- find --
 -- fzf and grep
-vim.keymap.set("n", "<leader>f", "", { desc = "fzf" }) --search cwd
+vim.keymap.set("n", "<leader>f", "", { desc = "fzf" })
 vim.keymap.set("n", "<leader>ff", function() require('fzf-lua').files() end, { desc = "fzf filenames" }) --search cwd
 vim.keymap.set("n", "<leader>fr", function() require('fzf-lua').resume() end, { desc = "repeat last fzf" }) --last search
 vim.keymap.set("n", "<leader>fh", function() require('fzf-lua').files({ cwd = '~/' }) end, { desc = "fzf ~/" }) --search home
-vim.keymap.set("n", "<leader>fc", function() require('fzf-lua').files({ cwd = '~/.config' }) end, { desc = "fzf ~/.config" }) --search .config
-vim.keymap.set("n", "<leader>fl", function() require('fzf-lua').files({ cwd = '~/src' }) end, { desc = "fzf ~/src" }) --search src
+vim.keymap.set("n", "<leader>fc", function() require('fzf-lua').files({ cwd = '~/.config' }) end, { desc = "fzf ~/.config" }) --search ~/.config
+vim.keymap.set("n", "<leader>fl", function() require('fzf-lua').files({ cwd = '~/src' }) end, { desc = "fzf ~/src" }) --search ~/src
+vim.keymap.set("n", "<leader>fd", function() require('fzf-lua').files({ cwd = '~/dev' }) end, { desc = "fzf ~/dev" }) --search ~/dev
 vim.keymap.set("n", "<leader>fg", function() require('fzf-lua').grep() end, { desc = "ripgrep" }) --grep
 vim.keymap.set("n", "<leader>fG", function() require('fzf-lua').grep_cword() end, { desc = "ripgrep word under cursor" }) --grep word under cursor
 
@@ -57,11 +98,12 @@ vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
 vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+vim.keymap.set("n", "K", ":m .-2<CR>J", { desc = "Join upwards" })
 -- treewalker swaps
-vim.keymap.set("n", "<C-S-k>", "<cmd>Treewalker SwapUp<cr>", { silent = true })
-vim.keymap.set("n", "<C-S-j>", "<cmd>Treewalker SwapDown<cr>", { silent = true })
-vim.keymap.set("n", "<C-S-h>", "<cmd>Treewalker SwapLeft<cr>", { silent = true })
-vim.keymap.set("n", "<C-S-l>", "<cmd>Treewalker SwapRight<cr>", { silent = true })
+vim.keymap.set("n", "<A-K>", "<cmd>Treewalker SwapUp<cr>", { silent = true })
+vim.keymap.set("n", "<A-J>", "<cmd>Treewalker SwapDown<cr>", { silent = true })
+vim.keymap.set("n", "<A-H>", "<cmd>Treewalker SwapLeft<cr>", { silent = true })
+vim.keymap.set("n", "<A-L>", "<cmd>Treewalker SwapRight<cr>", { silent = true })
 -- better indenting in visual mode
 vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
 vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
