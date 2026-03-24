c801677e817e7236f926a396fde3337538fe8b49
Arthur Petroff <apetbrz@gmail.com>
2026-03-24 19:18:06 -0400

    added manual format bind using lsp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -48,27 +48,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
 	group = vim.api.nvim_create_augroup('my.lsp', {}),
 	callback = function(ev)
 		local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
-		if client:supports_method('textDocument/implementation') then
-			-- Create a keymap for vim.lsp.buf.implementation ...
-		end
 		-- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
 		if client:supports_method('textDocument/completion') then
-			-- Optional: trigger autocompletion on EVERY keypress. May be slow!
-			-- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
-			-- client.server_capabilities.completionProvider.triggerCharacters = chars
 			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-		end
-		-- Auto-format ("lint") on save.
-		-- Usually not needed if server supports "textDocument/willSaveWaitUntil".
-		if not client:supports_method('textDocument/willSaveWaitUntil')
-			and client:supports_method('textDocument/formatting') then
-			vim.api.nvim_create_autocmd('BufWritePre', {
-				group = vim.api.nvim_create_augroup('my.lsp', {clear=false}),
-				buffer = ev.buf,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
-				end,
-			})
 		end
 	end,
 })
@@ -77,6 +59,15 @@ vim.keymap.set("n", "<leader>c", "", { desc = "Code Utils (LSP Server)" })
 vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, { desc = "Code Action" })
 vim.keymap.set("n", "<leader>cd", function() vim.lsp.buf.definition() end, { desc = "Go to Definition" })
 vim.keymap.set("n", "<leader>cD", function() vim.lsp.buf.declaration() end, { desc = "Go to Declaration" })
+vim.keymap.set("n", "<leader>cf", function() 
+	local client = assert(vim.lsp.get_client_by_id(1), "No LSP client!")
+	if client:supports_method('textDocument/formatting') then
+		vim.lsp.buf.format({ id = client.id, timeout_ms = 1000 })
+		print("Zap!")
+	else
+		print("Client does not support formatting!")
+	end
+end, { desc = "Format buffer" })
 vim.keymap.set("n", "<leader>ch", function() vim.lsp.buf.hover() end, { desc = "Hover Info" })
 vim.keymap.set("n", "<leader>ci", function() vim.lsp.buf.implementation() end, { desc = "Go to Implementation" })
 vim.keymap.set("n", "<leader>cn", function() vim.lsp.buf.rename() end, { desc = "Rename Symbol" })
